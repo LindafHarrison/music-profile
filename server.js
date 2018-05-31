cb073777c95ec0f3438bb16b5c829c643338ee47
Linda Harrison <lindafaithharrison@gmail.com>
2018-05-31 19:09:54 -0400

    heroku changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const listItemsController = require('./controllers/ListItemsController.js')
 
-// app.use(express.static('build'));
+app.use(express.static('build'));
 
 mongoose.connect('mongodb://paul:codesmith@ds139960.mlab.com:39960/music-profile');
 mongoose.connection.once('open', () => {
@@ -15,9 +15,9 @@ mongoose.connection.once('open', () => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile('login.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile('login.html');
+// });
 //route for login
 app.get('/', (req, res) => {
   res.redirect('process.env.BACKEND_URI || "http://localhost:8888"');
